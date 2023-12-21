4ea3c29d29e9767fe5058dd203d53dbf96c4edce
Mateosacco99 <mateosacco99@gmail.com>
2023-12-21 16:36:22 -0300

    0001

--- blogAPP/models.py
+++ blogAPP/models.py
@@ -8,7 +8,6 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    imagepost = models.ImageField(upload_to ='uploads/')
     body = models.TextField()
     date = models.DateField(auto_now_add=True, auto_now=False)
     
