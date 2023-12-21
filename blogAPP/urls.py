7bf283659a701e2483f2035f7d88611932535348
Mateosacco99 <mateosacco99@gmail.com>
2023-12-21 18:23:39 -0300

    0004

--- blogAPP/urls.py
+++ blogAPP/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('about/', views.about, name="about"),
     path('pages/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
-    path('add_post/', AddPostView.as_view(), name="add_post")
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('pages/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('pages/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
+
 
 ]
--- blogAPP/views.py
+++ blogAPP/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
+from .forms import PostForm, EditForm
 
-# Create your views here.
-
-# def home(request):
-#     return render(request, "home.html", {})
 
 class HomeView(ListView):
     model = Post
@@ -20,6 +17,14 @@ def about(request):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = "add_post.html"
-    fields = "__all__"
+    
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = "update_post.html"
 
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
