d4c8661315c5ca0c34c52fb6b35936706cdc3484
Mateosacco99 <mateosacco99@gmail.com>
2023-12-21 19:40:53 -0300

    0006 added user authentication

--- EntregaFinal/settings.py
+++ EntregaFinal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogAPP',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "blogAPP", "static")]
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
