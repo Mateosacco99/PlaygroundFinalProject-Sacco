ab076cd074a48e9fb094469b7eaae7b19afd90d0
Mateosacco99 <mateosacco99@gmail.com>
2023-12-22 02:01:03 -0300

    0010 User Edit forms

---
+++ accounts/forms.py
@@ -0,0 +1,35 @@
+from typing import Any
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
+    def __init__(self, *args: Any, **kwargs: Any):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['class']='form-control'
+        self.fields['password1'].widget.attrs['class']='form-control'
+        self.fields['password2'].widget.attrs['class']='form-control'
+        
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_login = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))    
+    # is_superuser = forms.CharField(max_length=25, widget=forms.CheckboxInput(attrs={'class':'form-ceck'}))
+    # is_staff = forms.CharField(max_length=25, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    # is_active = forms.CharField(max_length=25, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    # date_joined = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email',) #'password', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined',)
--- blogAPP/models.py
+++ blogAPP/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy, reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -9,7 +10,8 @@ class Post(models.Model):
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    texto = models.TextField()
+    #texto = models.TextField()
+    texto = RichTextField(blank=True, null=True)
     fecha = models.DateField(auto_now_add=True, auto_now=False)
     categoria = models.CharField(max_length=20, default='Sin Categoria')
     
